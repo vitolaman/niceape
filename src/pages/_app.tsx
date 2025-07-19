acd1d232852f73da18bcf4483075678e9e3227b7
Zafiel <clickbytedev@gmail.com>
2025-07-19 22:40:21 +0700

    feat: try change wallet adapter config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,12 +8,15 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useWindowWidthListener } from '@/lib/device';
 import { ThemeProvider } from '@/contexts/ThemeProvider';
 import { ToastProvider } from '@/contexts/ToastProvider';
+import { BackpackWalletAdapter } from '@solana/wallet-adapter-backpack';
 
 export default function App({ Component, pageProps }: AppProps) {
   const wallets: Adapter[] = useMemo(() => {
-    return [new PhantomWalletAdapter(), new SolflareWalletAdapter()].filter(
-      (item) => item && item.name && item.icon
-    ) as Adapter[];
+    return [
+      new BackpackWalletAdapter(),
+      new SolflareWalletAdapter(),
+      new PhantomWalletAdapter(),
+    ].filter((item) => item && item.name && item.icon) as Adapter[];
   }, []);
 
   const queryClient = useMemo(() => new QueryClient(), []);
@@ -30,10 +33,10 @@ export default function App({ Component, pageProps }: AppProps) {
               env: 'mainnet-beta',
               autoConnect: true,
               metadata: {
-                name: 'UnifiedWallet',
-                description: 'UnifiedWallet',
-                url: 'https://jup.ag',
-                iconUrls: ['https://jup.ag/favicon.ico'],
+                name: 'NiceApe',
+                description: 'Trade to Donate',
+                url: process.env.NEXT_PUBLIC_URL || 'https://jup.ag',
+                iconUrls: [process.env.NEXT_PUBLIC_URL + '/favicon.ico'],
               },
               // notificationCallback: WalletNotification,
               theme: 'dark',
